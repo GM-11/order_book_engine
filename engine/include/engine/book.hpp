1fc5051371bc9285d68a3dc8cef93a4f55262386
Gopal Mathur <mathuyrgopal@gmail.com>
2026-09-19 23:45:49 +0530

    Add match results and stop order support to book

--- engine/include/engine/book.hpp
+++ engine/include/engine/book.hpp
@@ -3,6 +3,7 @@
 #include "engine/level.hpp"
 #include "engine/order.hpp"
 #include "engine/pool.hpp"
+#include "engine/result.hpp"
 #include "engine/trade.hpp"
 #include <functional>
 #include <map>
@@ -17,18 +18,27 @@ struct ProposedFill {
     Quantity fill_quantity;
 };
 
+struct MatchPlan {
+    std::vector<ProposedFill> fills;
+    bool halted_by_self_trade = false;
+};
+
 class Book {
   public:
     explicit Book(std::size_t pool_capacity = 100000)
         : node_pool_(pool_capacity), next_trade_id_(1) {}
 
-    std::vector<Trade> add_order(Order order);
+    OrderResult add_order(Order order);
     bool cancel_order(OrderId order_id);
     std::optional<Price> best_bid() const;
     std::optional<Price> best_ask() const;
 
+    void place_stop_order(StopOrder stop_order);
+    bool cancel_stop_order(OrderId order_id);
+
   private:
-    std::vector<ProposedFill> plan_match(const Order &incoming) const;
+    MatchPlan plan_match(const Order &incoming) const;
+    void check_and_trigger_stops(Price last_trade_price);
     void unlink_and_maybe_erase_level(Node *node);
     template <typename SideMap>
     void unlink_and_maybe_erase_from_level(SideMap &side_map, Node *node);
@@ -38,6 +48,7 @@ class Book {
     std::map<Price, Level, std::greater<Price>> bids_;
     std::map<Price, Level> asks_;
     std::unordered_map<OrderId, Node *> id_index_;
+    std::vector<StopOrder> pending_stops_;
     NodePool node_pool_;
     TradeId next_trade_id_;
 };
